d4cbad7e09f394b0099e218a9087802265542e5b
TimeTetsuko <timetec_ios@yahoo.co.jp>
2017-01-25 15:19:53 +0900

    Initial Commit

---
+++ KyoyuTEST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KyoyuTEST
+//
+//  Created by timetec on 2017/01/25.
+//  Copyright © 2017年 timetec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
